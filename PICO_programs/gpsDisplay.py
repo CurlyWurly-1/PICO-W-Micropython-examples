3c750a09f998f3d8abcc57c8818d7649dc2683c6
CurlyWurly-1 <CurlyWurly-1@users.noreply.github.com>
2023-03-17 12:42:26 +0000

    Update gpsDisplay.py

--- PICO_programs/gpsDisplay.py
+++ PICO_programs/gpsDisplay.py
@@ -1,12 +1,16 @@
 # https://github.com/robert-hh/SH1106
+# Remember - The pin numbers are the GP numbers e.g. pin(10) is really GP10
+# Remember - Use the I2C1 bus - I2C0 seems to give "EIO" problems
+# Remember - If you have "EIO" problems, try disconnecting and reconnecting power to PICO, and then re-execute program
+
 from machine import UART, Pin
 from machine import Pin, I2C
 import sh1106
 from time import sleep
 
-sda = Pin(0)
-scl = Pin(1)
-i2c = I2C(0, scl=scl, sda=sda, freq=400000)
+sda = Pin(10)
+scl = Pin(11)
+i2c = I2C(1, scl=scl, sda=sda, freq=400000)
 #print("I2C Address      : "+hex(i2c.scan()[0]).upper())
 #print("I2C Configuration: "+str(i2c))
 oled = sh1106.SH1106_I2C(128, 64, i2c, Pin(16), 0x3c)
