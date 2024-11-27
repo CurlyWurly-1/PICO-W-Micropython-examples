d7ecf0733cc525ef5a44c94c094da8030bb7c0ba
CurlyWurly-1 <CurlyWurly-1@users.noreply.github.com>
2024-11-27 13:23:42 +0000

    Create catfacts.py

---
+++ PICO_programs/catfacts.py
@@ -0,0 +1,24 @@
+"""
+Get a cat fact from the internet!
+You will need to add your wireless SSID and password to secrets.py (and save this file to your Pico in library called "lib")
+"""
+
+import network
+import requests
+from secrets import secrets
+from time import sleep
+
+# connect to wifi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+ssid = secrets['ssid']
+pw = secrets['pw']
+wlan.connect(ssid, pw)
+while wlan.isconnected() is False:
+    print('Waiting for connection...')
+    sleep(1)
+
+request = requests.get('http://catfact.ninja/fact').json()
+fact = request['fact']
+print('Cat fact!')
+print(fact)
