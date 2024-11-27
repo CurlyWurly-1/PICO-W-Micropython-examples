65e3912eff5f1c50f2ad7a198e713f6982a4e7f4
CurlyWurly-1 <CurlyWurly-1@users.noreply.github.com>
2024-11-27 13:20:51 +0000

    Update weatherStation.py

--- PICO_programs/weatherStation.py
+++ PICO_programs/weatherStation.py
@@ -1,33 +1,38 @@
-from machine import Pin, I2C        #importing relevant modules & classes
+# Please note:
+#   When connected, the IP address will be printed (e.g. 192.168.X.YYY)
+#   To see the values of the sensor, use a browser and access URL "http://192.168.X.YYY" 
 
-from time import sleep
-
-import utime
-import socket
 import network
+import socket
+from secrets import secrets
+from time import sleep
+from machine import Pin, I2C        #importing relevant modules & classes
 import bme280                       #importing BME280 library
 
 # Function to load in html page    
 def get_html(html_name):
     with open(html_name, 'r') as file:
         html = file.read()
-        
     return html
 
-
-
+# Set up I2C using pins 6 and 7
 i2c=I2C(1,sda=Pin(6), scl=Pin(7), freq=400000)
 
+# Load login data from different file for safety reasons
+ssid = secrets['ssid']
+pw = secrets['pw']
+
+# Access WiFi
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect("BT-Wifi1","q1!3£4$5%")
+wlan.connect(ssid,pw)
 
 # Wait for connect or fail
-wait = 10
-while wait > 0:
+max_wait = 10
+while max_wait > 0:
     if wlan.status() < 0 or wlan.status() >= 3:
         break
-    wait -= 1
+    max_wait -= 1
     print('waiting for connection...')
     sleep(1)
 
@@ -53,23 +58,35 @@ while True:
         print('client connected from', addr)
         request = conn.recv(1024)
         conn.settimeout(None)
-        # HTTP-Request receive
+
+# HTTP-Request receive
         print('Request:', request)              
-        # HTTP-Response send
-#        response = web_page()
-        bme = bme280.BME280(i2c=i2c)    
+
+            
+# Retrieve HTML template
         response = get_html('windex.html')
+
+# Get values from sensor. If not connected, use default values [1,2,3]
+# and replace the three "placeholders" with the corresponding variable values 
+        try:
+            bme = bme280.BME280(i2c=i2c)
+            vals = bme.values
+        except:
+            vals =  [1,2,3]
+        response = response.replace("%%b1", str(vals[0]) )
+        response = response.replace("%%b2", str(vals[1]) )
+        response = response.replace("%%b3", str(vals[2]) )
+
+# Toggle Heartbeat indicator 
         if hbeat == False:
             hbeat = True
             response = response.replace("%%i1", '*' )   
         else:
             hbeat = False
-            response = response.replace("%%i1", ' ' )   
-        response = response.replace("%%b1", str(bme.values[0]) )
-        response = response.replace("%%b2", str(bme.values[1]) )
-        response = response.replace("%%b3", str(bme.values[2]) )
+            response = response.replace("%%i1", ' ' )
+
+# Send response back
         conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-#        conn.send('HTTP/1.0 200 OKrnContent-type: text/htmlrnrn')
         conn.sendall(response)
         conn.close()
     except OSError as e:
